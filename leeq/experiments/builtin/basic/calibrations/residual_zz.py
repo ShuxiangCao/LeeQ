dceedf34f459ebe55f245fbde20c8fcbb0fd7b05
Shuxiang Cao <shuxiang.cao@physics.ox.ac.uk>
2023-12-06 16:07:13 +0000

    func: Update residual ZZ calibration code

--- leeq/experiments/builtin/basic/calibrations/residual_zz.py
+++ leeq/experiments/builtin/basic/calibrations/residual_zz.py
@@ -63,26 +63,24 @@ class CalibrateOptimizedFrequencyWith2QZZShift(Experiment):
         c1q1.update_freq(c1q1['X'].freq + frequency_change / 2)
         c1q2.update_freq(c1q2['X'].freq + frequency_change / 2)
 
-
 class ZZShiftTwoQubitMultilevel(Experiment):
     """Class to compute ZZ Shift for Two Qubit Multilevel system."""
 
     @log_and_record
     def run(self,
             duts: List[TransmonElement],
-            name: str,
-            mprim_index: int,
+            collection_name: str = 'f01',
+            mprim_index: int = 0,
             start: float = 0.0,
             stop: float = 1,
             step: float = 0.005,
             set_offset: int = 10,
-            disable_sub_plot: bool = False,
-            hardware_stall: bool = False) -> None:
+            disable_sub_plot: bool = False) -> None:
         """Run the ZZ Shift experiment.
 
         Parameters:
             duts: The DUTs (Device Under Test).
-            name: The name of the frequency collection (e.g., 'f01').
+            collection_name: The name of the frequency collection (e.g., 'f01').
             mprim_index: The index of the measurement primitive.
             start: The start frequency for the sweep.
             stop: The stop frequency for the sweep.
@@ -102,26 +100,26 @@ class ZZShiftTwoQubitMultilevel(Experiment):
         if disable_sub_plot:
             setup().status().set_param("Plot_Result_In_Jupyter", False)
 
-        c1q1 = duts[0].get_c1(name)
-        c1q2 = duts[1].get_c1(name)
+        c1q1 = duts[0].get_c1(collection_name)
+        c1q2 = duts[1].get_c1(collection_name)
 
         # Q1 ramsey Q2 steady
         self.q1_ramsey_q2_ground = SimpleRamseyMultilevel(
-            duts[0], name=name, mprim_index=mprim_index, initial_lpb=None, start=start, stop=stop, step=step,
-            set_offset=set_offset, update=False, hardware_stall=hardware_stall)
+            duts[0], collection_name=collection_name, mprim_index=mprim_index, initial_lpb=None, start=start, stop=stop, step=step,
+            set_offset=set_offset, update=False)
 
         self.q1_ramsey_q2_excited = SimpleRamseyMultilevel(
-            duts[0], name=name, mprim_index=mprim_index, initial_lpb=c1q2['X'], start=start, stop=stop, step=step,
-            set_offset=set_offset, update=False, hardware_stall=hardware_stall)
+            duts[0], collection_name=collection_name, mprim_index=mprim_index, initial_lpb=c1q2['X'], start=start, stop=stop, step=step,
+            set_offset=set_offset, update=False)
 
         # Q2 ramsey Q1 steady
         self.q2_ramsey_q1_ground = SimpleRamseyMultilevel(
-            duts[1], name=name, mprim_index=mprim_index, initial_lpb=None, start=start, stop=stop, step=step,
-            set_offset=set_offset, update=False, hardware_stall=hardware_stall)
+            duts[1], collection_name=collection_name, mprim_index=mprim_index, initial_lpb=None, start=start, stop=stop, step=step,
+            set_offset=set_offset, update=False)
 
         self.q2_ramsey_q1_excited = SimpleRamseyMultilevel(
-            duts[1], name=name, mprim_index=mprim_index, initial_lpb=c1q1['X'], start=start, stop=stop, step=step,
-            set_offset=set_offset, update=False, hardware_stall=hardware_stall)
+            duts[1], collection_name=collection_name, mprim_index=mprim_index, initial_lpb=c1q1['X'], start=start, stop=stop, step=step,
+            set_offset=set_offset, update=False)
 
         self.zz = [
             self.q1_ramsey_q2_excited.frequency_guess - self.q1_ramsey_q2_ground.frequency_guess,
@@ -129,3 +127,4 @@ class ZZShiftTwoQubitMultilevel(Experiment):
         ]
 
         setup().status().set_param("Plot_Result_In_Jupyter", plot_result_in_jupyter)
+
