b12365fcd3abcc7829822b1ef15941380eab8335
Shuxiang Cao <shuxiang.cao@physics.ox.ac.uk>
2024-06-14 15:20:03 +0100

    dev: Implement pygsti adapter

--- leeq/experiments/integrations/pygsti/base.py
+++ leeq/experiments/integrations/pygsti/base.py
@@ -1,124 +1,180 @@
-from leeq import Experiment
-from leeq.core.primitives.logical_primitives import LogicalPrimitiveBlockSerial, LogicalPrimitiveBlockParallel
+from typing import List
+
+import numpy as np
+from labchronicle import register_browser_function
+
+import leeq.theory.utils
+from leeq import Experiment, Sweeper, basic_run
+from leeq.core.primitives.logical_primitives import LogicalPrimitiveBlockSerial, LogicalPrimitiveBlockParallel, \
+    LogicalPrimitiveBlockSweep
 from leeq.utils import setup_logging
+from leeq.utils.compatibility import *
+
+__all__ = ['PyGSTiExperiment', 'PyGSTiRBExperiment']
 
 logger = setup_logging(__name__)
 
-
-def gate_name_to_lpb_single_qubit(label, dut):
-    gate_lpbs = []
-
-    if isinstance(label, str):
-        c1 = dut.get_default_c1()
-        gate_at_ith_qubit = label[0].upper()
-        if gate_at_ith_qubit == 'I':
-            return c1['I']
-        else:
-            return c1[gate_at_ith_qubit + 'p']
-    else:
-        for i in range(len(label)):
-            c1 = duts[i].get_default_c1()
-            gate_at_ith_qubit = label[i].upper()
-            if gate_at_ith_qubit == 'I':
-                continue
-            gate_lpbs.append(c1[gate_at_ith_qubit + 'p'])
-
-    if len(gate_lpbs) == 1:
-        return gate_lpbs[0]
-
-    return prims.ParallelLPB(gate_lpbs)
-
-
-def pygsti_design_to_lpbs(exp_design, duts, mprim_indexes, gate_name_to_lpb_func):
-    list_of_experiments = exp_design.all_circuits_needing_data
-
-    lpb_sequences = []
-
-    mprims = [duts[i].get_measurement_prim_intlist(mprim_indexes[i]) for i in range(len(duts))]
-    lpb_sequences = []
-
-    for circuit in list_of_experiments:
-        depth = circuit.num_layers
-        lpb = []
-
-        layer = circuit.to_label()
-        layer_lpb = []
-        for gate in layer.components:
-            gate_name = gate.name
-            if gate_name == 'COMPOUND':
-                continue
-
-            qubit_index = gate.qubits
-            if qubit_index is None:
-                qubit_index = 0
-            layer_lpb.append(gate_name_to_lpb_func(gate_name, duts, qubit_index))
-
-        if len(layer_lpb) == 0:
-            lpb.append(duts[0].get_default_c1()['I'])
-        elif len(layer_lpb) == 1:
-            lpb.append(layer_lpb[0])
-        else:
-            lpb.append(LogicalPrimitiveBlockParallel(layer_lpb))
-
-        if len(lpb) == 0:
-            lpb = [duts[0].get_default_c1()['I']]
-
-        lpb = LogicalPrimitiveBlockSerial(lpb) if len(lpb) > 1 else lpb[0]
-        lpb = lpb + LogicalPrimitiveBlockParallel(mprims)
-        lpb_sequences.append(lpb)
-
-    return lpb_sequences, mprims
+from typing import List, Any, Tuple, Callable
 
 
 class PyGSTiExperiment(Experiment):
+    """
+    A class for handling pyGSTi-based quantum experiments, focusing on experiment setup,
+    data collection, and processing. Inherits from a generic Experiment class.
+    """
 
-    def _construct_lpbs(self, ):
+    def _pygsti_design_to_lpbs(self, exp_design: "pygsti design",
+                               duts: List['TransmonElements'],
+                               mprim_indexes: List[int],
+                               gate_name_to_lpb_func: Callable) -> Tuple[List[Any], List[Any]]:
+        """
+        Converts a pyGSTi design to a list of logical primitive blocks (LPBs).
+
+        Args:
+        exp_design: The experiment design from pyGSTi.
+        duts: Devices under test, typically quantum elements like transmons.
+        mprim_indexes: Indices of measurement primitives in the devices.
+        gate_name_to_lpb_func: A function that maps gate names to LPBs.
+
+        Returns:
+        A tuple containing the list of LPB sequences and a list of measurement primitives.
+        """
+        list_of_experiments = exp_design.all_circuits_needing_data
+        self.list_of_experiments = list_of_experiments
+
+        mprims = [duts[i].get_measurement_prim_intlist(mprim_indexes[i]) for i in range(len(duts))]
+        lpb_sequences = []
+
+        for circuit in list_of_experiments:
+            depth = circuit.num_layers
+            lpb = []
+
+            layer = circuit.to_label()
+            layer_lpb = []
+            for gate in layer.components:
+                gate_name = gate.name
+                if gate_name == 'COMPOUND':
+                    continue
+
+                qubit_index = gate.qubits
+                if qubit_index is None:
+                    qubit_index = 0
+                layer_lpb.append(gate_name_to_lpb_func(gate_name, duts, qubit_index))
+
+            if len(layer_lpb) == 0:
+                lpb.append(duts[0].get_default_c1()['I'])
+            elif len(layer_lpb) == 1:
+                lpb.append(layer_lpb[0])
+            else:
+                lpb.append(LogicalPrimitiveBlockSerial(layer_lpb))
+
+            if len(lpb) == 0:
+                lpb = [duts[0].get_default_c1()['I']]
+
+            lpb = LogicalPrimitiveBlockSerial(lpb) if len(lpb) > 1 else lpb[0]
+            lpb = lpb + LogicalPrimitiveBlockParallel(mprims)
+            lpb_sequences.append(lpb)
+
+        return lpb_sequences, mprims
+
+    def _construct_lpbs(self):
+        """
+        Placeholder for LPB construction logic.
+        """
         pass
 
-    def run(self, design, duts, mprim_indexes):
+    def _process_collected_data(self):
+        """
+        Processes collected data using pyGSTi and updates the internal state.
+        """
+        import pygsti
+
+        self.ds = pygsti.data.DataSet(outcome_labels=self.outcome_labels)
+
+        for i, circuit in enumerate(self.list_of_experiments):
+            self.ds.add_count_dict(circuit,
+                                   {label: self.sample_counts[j, i] for j, label in enumerate(self.outcome_labels)})
+
+        self.protocol_data = pygsti.protocols.ProtocolData(edesign=self.exp_design, dataset=self.ds)
+
+    def _build_outcome_labels(self, duts_count: int):
+        """
+        Builds a list of outcome labels based on the number of devices.
+
+        Args:
+        duts_count: The number of devices under test.
+        """
+        self.outcome_labels = ['0', '1']
+
+        for i in range(duts_count - 1):
+            self.outcome_labels = [x + '0' for x in self.outcome_labels] + [x + '1' for x in self.outcome_labels]
+
+    def run(self, design, duts: List['TransmonElements'], mprim_indexes: List[int]):
+        """
+        Main method to run the experiment with the given design and devices.
+
+        Args:
+        design: The experimental design to use.
+        duts: List of devices under test.
+        mprim_indexes: List of measurement primitive indexes.
+        """
         from pygsti.protocols import CircuitListsDesign
 
         if not isinstance(design, CircuitListsDesign):
-            msg = "design must be a pyGSTi Design object"
-            raise ValueError(msg)
+            raise ValueError("Design must be a pyGSTi Design object")
 
         self.duts = duts
+        self.exp_design = design
         self._construct_lpbs()
-        lpbs, mprims = pygsti_design_to_lpbs(exp_design=design,
-                                             duts=duts,
-                                             mprim_indexes=mprim_indexes,
-                                             gate_name_to_lpb_func=self.get_gate_lpb
-                                             )
+        self._build_outcome_labels(len(duts))
+        lpbs, mprims = self._pygsti_design_to_lpbs(exp_design=design,
+                                                   duts=duts,
+                                                   mprim_indexes=mprim_indexes,
+                                                   gate_name_to_lpb_func=self.get_gate_lpb)
+
+        sweep_lpb = LogicalPrimitiveBlockSweep(lpbs)
+        swp = Sweeper.from_sweep_lpb(sweep_lpb)
+
+        final_lpb = sweep_lpb
+
+        basic_run(final_lpb, swp, "<zs>")
+
+        self.result = np.dstack([mprim.result() for mprim in mprims]).transpose([2, 0, 1])
+        self.sample_counts = leeq.theory.utils.to_dense_sample_counts(self.result)
+        self._process_collected_data()
+
+    @register_browser_function()
+    def process_data_and_visualize(self):
+        """
+        Placeholder for data processing and visualization logic.
+        """
+        pass
 
 
-class PyGSTiExperimentQubitGates(PyGSTiExperiment):
+class PyGSTiRBExperiment(PyGSTiExperiment):
+    """
+    A subclass to handle Randomized Benchmarking (RB) experiments using pyGSTi.
+    """
 
-    def _construct_lpbs(self):
-        self._lpbs = []
-        duts = self.duts
-        for dut in duts:
-            c1 = dut.get_c1('f01')
-            self._lpbs.append({
-                'Gxpi2': c1['Xp'],
-                'Gxmpi2': c1['Xm'],
-                'Gypi2': c1['Yp'],
-                'Gympi2': c1['Ym']
-            })
+    def process_rb_data(self):
+        """
+        Processes RB data and returns the results.
 
-    def get_gate_lpb(self, gate_name, duts, qubit_index):
+        Returns:
+        RB results object.
+        """
+        import pygsti
+        protocol = pygsti.protocols.RB()
+        self.rb_results = protocol.run(self.protocol_data)
+        return self.rb_results
 
-        label = gate_name.__str__()
+    def visualize_rb_results(self):
+        """
+        Visualizes the RB results using pyGSTi's built-in visualization tools.
+        """
+        import pygsti
+        ws = pygsti.report.Workspace()
+        plot = ws.RandomizedBenchmarkingPlot(self.rb_results)
 
-        gate_lpbs = []
-
-        if isinstance(label, str):
-            return self._lpbs[0][label]
-        else:
-            for i in range(len(label)):
-                lpb_map = self._lpbs[i]
-                gate_lpbs.append(lpb_map[label])
-
-        if len(gate_lpbs) == 1:
-            return gate_lpbs[0]
-
-        return LogicalPrimitiveBlockParallel(gate_lpbs)
+        for fig in plot.figs:
+            fig.plotlyfig.show()
