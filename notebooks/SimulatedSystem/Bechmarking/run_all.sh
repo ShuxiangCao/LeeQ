c143da307057bcdb626e58910c82797c7f96b476
Shuxiang <shuxiang.cao@scao.me>
2024-07-23 12:00:47 +0100

    dev: Update embedding search benchmarking

--- notebooks/SimulatedSystem/Bechmarking/run_all.sh
+++ notebooks/SimulatedSystem/Bechmarking/run_all.sh
@@ -9,18 +9,17 @@ models=('replicate/meta/meta-llama-3-70b-instruct' 'replicate/meta/meta-llama-3-
 #models=('gpt-4o' 'gpt-4o-mini' 'gpt-4-turbo' 'gpt-4' 'claude-3-5-sonnet-20240620' 'claude-3-haiku-20240307' 'claude-3-opus-20240229' 'gemini-1.5-pro' 'gemini-1.5-flash' 'replicate/meta/meta-llama-3-70b-instruct' 'replicate/meta/meta-llama-3-8b-instruct')
 
 # Number of shots, you can adjust this value as needed
-shots=100
+shots=1
 
 # Create a directory for logs if it does not exist
 mkdir -p benchmark_logs
 
-model_path = $(echo "$model" | sed 's/\//_/g')
-
 # Run experiments in parallel
 for model in "${models[@]}"
 do
+    model_path=$(echo "$model" | sed 's/\//_/g')
     echo "Running $benchmark with model $model"
-    python embedding_search_benchmarking.py $model $shots > "benchmark_logs/embedding_search_${model_path}.out" 2> "benchmark_logs/embedding_search__${model_path}.err" &
+    python embedding_search_benchmarking.py "$model" "$shots" > "benchmark_logs/embedding_search_${model_path}.out" 2> "benchmark_logs/embedding_search_${model_path}.err" &
 done
 
 # Wait for all processes to finish
