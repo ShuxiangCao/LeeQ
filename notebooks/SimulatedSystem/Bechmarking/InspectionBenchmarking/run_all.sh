e62372cee82d4d5a25d8a6f532e4c8b58828dc9c
Shuxiang Cao <shuxiang.cao@physics.ox.ac.uk>
2024-07-23 10:01:09 +0100

    dev: Update scripts for benchmarking

--- notebooks/SimulatedSystem/Bechmarking/InspectionBenchmarking/run_all.sh
+++ notebooks/SimulatedSystem/Bechmarking/InspectionBenchmarking/run_all.sh
@@ -7,7 +7,7 @@ benchmarks=('resonator_spec' 'gmm' 'rabi' 'drag')
 models=('openai' 'anthropic' 'gemini' 'llama')
 
 # Number of shots, you can adjust this value as needed
-shots=2
+shots=100
 
 # Create a directory for logs if it does not exist
 mkdir -p benchmark_logs
@@ -19,7 +19,6 @@ do
     do
         echo "Running $benchmark with model $model"
         python run_benchmarks.py $benchmark $model $shots > "benchmark_logs/${benchmark}_${model}.out" 2> "benchmark_logs/${benchmark}_${model}.err" &
-        sleep 2
     done
 done
 
