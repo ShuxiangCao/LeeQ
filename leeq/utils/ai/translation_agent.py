a6a765be304a3f7d8eb5d64247d88eb818f35bb8
Zijian Zhang <doomspec@outlook.com>
2024-11-17 00:31:01 -0500

    dev: change the way of init trans agents

--- leeq/utils/ai/translation_agent.py
+++ leeq/utils/ai/translation_agent.py
@@ -1,5 +1,5 @@
 import os
-from k_agents.translation.agent import init_translation_agents
+from k_agents.translation.agent import prepare_translation_agents
 from k_agents.translation.env import TranslationAgentEnv
 from k_agents.variable_table import VariableTable
 
@@ -15,8 +15,7 @@ def init_leeq_translation_agents(document_root: str = "/procedures", n_agents_to
     def add_class_to_var_table(var_table, exp_cls):
         var_table.add_variable(exp_cls.__name__, exp_cls, exp_cls.__name__)
 
-    translation_agents, translation_var_table = init_translation_agents(builtin, document_root, n_agents_to_call, lambda_is_leeq_ai_exp_class, add_class_to_var_table)
-    translation_var_table: VariableTable
+    translation_agents, translation_var_table = prepare_translation_agents(builtin, document_root, n_agents_to_call, lambda_is_leeq_ai_exp_class, add_class_to_var_table)
     env = TranslationAgentEnv()
     env.translation_agents = translation_agents
     env.translation_var_table = translation_var_table
